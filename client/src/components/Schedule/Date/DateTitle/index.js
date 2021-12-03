99f138d8c6921d69098f721a922859465ed047c3
Bailee Dastugue <bailee.dast@gmail.com>
2021-12-03 16:36:19 -0600

    changed date title styling to not inherit absolute positioning

--- client/src/components/Schedule/Date/DateTitle/index.js
+++ client/src/components/Schedule/Date/DateTitle/index.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import getMonth from '../../../../utils/getMonth';
-import { makeStyles } from '@mui/styles';
 import { Typography, Button } from '@mui/material';
-
-const useStyles = makeStyles({
-  date: {
-    textTransform: 'none',
-    padding: '0 48px',
-    '&:hover': {
-      fontStyle: 'italic',
-      cursor: 'pointer',
-    },
-  },
-  button: {
-    '&:hover': {
-      backgroundColor: 'rgba(0, 0, 0, 0) !important',
-    },
-  },
-});
 
 const DateTitle = ({ date, onClick }) => {
   const newDate = new Date(date);
-  const classes = useStyles();
   return (
-    <Button className={classes.button}>
+    <Button
+      sx={{
+        '&:hover': {
+          backgroundColor: 'rgba(0, 0, 0, 0) !important',
+        },
+      }}
+    >
       <Typography
         variant='h5'
         onClick={onClick}
         component='h2'
-        className={classes.date}
         my={2}
-        sx={{ display: 'block' }}
+        sx={{
+          textTransform: 'none',
+          padding: '0 48px',
+          '&:hover': {
+            fontStyle: 'italic',
+            cursor: 'pointer',
+          },
+        }}
       >
         {getMonth(newDate.getMonth())} {newDate.getDate()},{' '}
         {newDate.getFullYear()}
